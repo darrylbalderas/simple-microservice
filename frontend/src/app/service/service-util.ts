e18abe264802d7e4b77775e9a500aa12227e61c9
darrylbalderas <drrylbalderas@gmail.com>
2018-10-08 08:04:53 -0700

    Changing a host names
    Signed-off-by: darrylbalderas <drrylbalderas@gmail.com>

--- frontend/src/app/service/service-util.ts
+++ frontend/src/app/service/service-util.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 
 export class Service {
-  public static SERVER_URL_PREFIX = 'http://localhost:8081/';
+  public static SERVER_URL_PREFIX = 'http://localhost:8080/';
 
   public static getHeaderWithJsonContentType() {
     const headers = new HttpHeaders({
