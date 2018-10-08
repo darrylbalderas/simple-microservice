6e5befb67e14eba07e171122ca5e13a130b59dac
darrylbalderas <drrylbalderas@gmail.com>
2018-10-08 09:28:10 -0700

    Create tags for docker images
    Signed-off-by: darrylbalderas <drrylbalderas@gmail.com>

--- backend/.run-docker.sh
+++ backend/.run-docker.sh
@@ -2,6 +2,9 @@
 
 gradle clean build
 
-docker build -t backend .
+docker build -t darryl3223/backend .
 
-docker run --rm --name backend -p 8080:8080 backend
+#Make sure you login into docker.io  =>  docker login docker.io
+docker push docker.io/darryl3223/backend
+
+# docker run --rm --name backend -p 8080:8080 darryl3223/backend
--- frontend/.run-docker.sh
+++ frontend/.run-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker build -t frontend .
+docker build -t darryl3223/frontend .
 
-docker run --rm -p 4200:4200 frontend
+docker push docker.io/darryl3223/frontend
+
+# docker run --rm -p 4200:4200 darryl3223/frontend
