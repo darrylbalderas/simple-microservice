b2fc1f31149dd4e0aad465bbfcabc7788b48eb9a
darrylbalderas <drrylbalderas@gmail.com>
2018-10-08 22:38:36 -0700

    Change deployment to stateful set in backend
    Signed-off-by: darrylbalderas <drrylbalderas@gmail.com>

--- backend/.run-docker.sh
+++ backend/.run-docker.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+# Creates jar file
 gradle clean build
 
+#Create backend image
 docker build -t darryl3223/backend .
 
-#Make sure you login into docker.io  =>  docker login docker.io
+# Pushing docker image to docker hub
 docker push docker.io/darryl3223/backend
 
+# Running on local kubernetes cluster 
+# Create statefulset and service
+kubectl apply -f deployment.yaml
+
+# Wait for pod to start up
+echo "Wait for pod to startup"
+sleep 6
+
+# Port forward backend service to localhost:4200
+kubectl port-forward backend-0 8080:8080
+
+# Running locally
 # docker run --rm --name backend -p 8080:8080 darryl3223/backend
--- frontend/.run-docker.sh
+++ frontend/.run-docker.sh
@@ -10,8 +10,12 @@ docker push docker.io/darryl3223/frontend
 # Creates statefulset and service
 kubectl apply -f deployment.yaml
 
+# Wait for pod to start up
+echo "Wait for pod to startup"
+sleep 6
+
 # Port forward frontend service to localhost:4200
 kubectl port-forward frontend-0 4200:4200
 
 # Running locally
-# docker run --rm -p 4200:4200 darryl3223/frontend
+# docker run --rm --name frontend -p 4200:4200 darryl3223/frontend
